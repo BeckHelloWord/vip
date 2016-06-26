8e93c4f148ed5ea1586ea8a30a70c8442c23d15f
缺心眼 <yuwei_shiny@163.com>
2016-06-26 17:07:08 +0800

    js优化
    增加跳转，修改title,优化样式

--- vip-app.js
+++ vip-app.js
@@ -3,28 +3,50 @@
  */
 (function () {
 
-    //主页个人信息
-    var personInfo={
-        integral:'2224',
-        name:'David Beckham',
-        headimg:'',
-        grade:'银象',
-        crownimg:''
-    };
+	//主页个人信息
+	var personInfo = {
+		integral: '1111',
+		name: 'David Beckham',
+		headimg: '',
+		grade: '银象',
+		crownimg: ''
+	};
 
-    var vip=new Vue({
-        el:'#vip-page',
-        data:{
-            showPage:'mainPage',
-            homepage:personInfo,    //主页信息
-        },
-        methods:{
-            skipIntegral:function () {
-                this.showPage='history'
-            },
-            skipGrade:function () {
+	var titleTxt = '宝象会员';
 
-            }
-        }
-    })
+	var vip = new Vue({
+		el: '#vip-page',
+		data: {
+			showPage: 'mainPage',
+			homepage: personInfo, //主页信息
+		},
+		methods: {
+			//跳转
+			skip: function (type) {
+				switch (type) {
+				case 'history':
+					this.showPage = 'history';
+					topNav.titleInfo='成长值记录';
+					break;
+				case 'growUp':
+					this.showPage = 'growUp';
+					topNav.titleInfo='成长值帮助';
+					break;
+				case 'power':
+					this.showPage = 'power';
+					topNav.titleInfo='权益介绍';
+					break;
+				}
+
+			}
+		}
+	});
+
+	var topNav = new Vue({
+		el: '#topNav',
+		data: {
+			titleInfo: titleTxt
+		}
+	})
+
 })();
